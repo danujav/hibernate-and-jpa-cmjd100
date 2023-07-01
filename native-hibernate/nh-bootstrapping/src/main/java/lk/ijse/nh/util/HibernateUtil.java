c5ce959d05031665f3554c38ee31c03cdcb37e10
danu <danujagreru@gmail.com>
2023-07-01 10:46:40 +0530

    create buildSessionFactory method

--- native-hibernate/nh-bootstrapping/src/main/java/lk/ijse/nh/util/HibernateUtil.java
+++ native-hibernate/nh-bootstrapping/src/main/java/lk/ijse/nh/util/HibernateUtil.java
@@ -6,12 +6,29 @@ package lk.ijse.nh.util;
 */
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateUtil {
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
-        return null;    //session factory object
+        StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                .configure("")
+                .build();
+
+        Metadata metadata = new MetadataSources(standardRegistry)
+                .getMetadataBuilder()
+                .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+                .build();
+
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder()
+                .build();
+
+        return sessionFactory;
     }
 
     public static SessionFactory getSessionFactory() {
