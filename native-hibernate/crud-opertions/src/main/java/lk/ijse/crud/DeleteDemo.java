c6f6bf610cc3de18e44b9e03077279e8c6c6800e
danu <danujagreru@gmail.com>
2023-07-01 13:22:39 +0530

    delete() complete

---
+++ native-hibernate/crud-opertions/src/main/java/lk/ijse/crud/DeleteDemo.java
@@ -0,0 +1,28 @@
+package lk.ijse.crud;
+
+/*
+    @author DanujaV
+    @created 7/1/23 - 12:44 PM   
+*/
+
+import lk.ijse.crud.entity.Item;
+import lk.ijse.crud.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+public class DeleteDemo {
+    public static void main(String[] args) {
+        try(SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+            Session session = sessionFactory.openSession()) {
+
+            Transaction transaction = session.beginTransaction();
+
+            Item i1 = session.find(Item.class, 5);
+            session.remove(i1);
+
+
+            transaction.commit();
+        }
+    }
+}
