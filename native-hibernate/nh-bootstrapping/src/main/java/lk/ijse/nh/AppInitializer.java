669eada45854247f6b45e22b0b925ab91ee0a14e
danu <danujagreru@gmail.com>
2023-07-01 11:24:29 +0530

    open a session

--- native-hibernate/nh-bootstrapping/src/main/java/lk/ijse/nh/AppInitializer.java
+++ native-hibernate/nh-bootstrapping/src/main/java/lk/ijse/nh/AppInitializer.java
@@ -5,8 +5,20 @@ package lk.ijse.nh;
     @created 7/1/23 - 10:15 AM   
 */
 
+import lk.ijse.nh.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 public class AppInitializer {
     public static void main(String[] args) {
-        
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        System.out.println("------");
+
+        System.out.println("session: " + session);
+
+        session.close();
+        sessionFactory.close();
     }
 }
