e750cbe8145db1de568d63b1741fb5d4aebb3e91
danu <danujagreru@gmail.com>
2023-07-01 13:14:13 +0530

    detached state

--- native-hibernate/crud-opertions/src/main/java/lk/ijse/crud/DetachStateDemo.java
+++ native-hibernate/crud-opertions/src/main/java/lk/ijse/crud/DetachStateDemo.java
@@ -2,7 +2,7 @@ package lk.ijse.crud;
 
 /*
     @author DanujaV
-    @created 7/1/23 - 12:44 PM   
+    @created 7/1/23 - 1:07 PM   
 */
 
 import lk.ijse.crud.entity.Item;
@@ -11,7 +11,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
-public class UpdateDemo {
+public class DetachStateDemo {
     public static void main(String[] args) {
         try(SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
             Session session = sessionFactory.openSession()) {
@@ -19,8 +19,13 @@ public class UpdateDemo {
             Transaction transaction = session.beginTransaction();
 
             Item i1 = session.find(Item.class, 4);
-            i1.setDescription("Choco");
-            i1.setUnitPrice(1000);
+            System.out.println("is i1 is persistence state: " + session.contains(i1));  //true
+
+            session.detach(i1);
+            System.out.println("detached here");
+
+            i1.setDescription("Toffee");
+            System.out.println("is i1 is persistence state: " + session.contains(i1));  //false
 
             transaction.commit();
         }
