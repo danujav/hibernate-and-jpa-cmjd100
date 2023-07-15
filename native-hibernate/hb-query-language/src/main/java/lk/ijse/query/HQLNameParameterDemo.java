b98ad815188fbdeda8759f2710044b253a703c21
danu <danujagreru@gmail.com>
2023-07-15 16:29:26 +0530

    name parameters

---
+++ native-hibernate/hb-query-language/src/main/java/lk/ijse/query/HQLNameParameterDemo.java
@@ -0,0 +1,36 @@
+package lk.ijse.query;
+
+/*
+    @author DanujaV
+    @created 7/15/23 - 3:48 PM   
+*/
+
+import lk.ijse.query.entity.Customer;
+import lk.ijse.query.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class HQLNameParameterDemo {
+    public static void main(String[] args) {
+        try (SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+             Session session = sessionFactory.openSession()) {
+
+            /*Customer customer = session.find(Customer.class, "C001");
+            System.out.println(customer);*/
+            //name parameters
+            String name = "Supun";
+            String hql = "FROM CustomerDetail WHERE name = :cName";
+
+            Query query = session.createQuery(hql);
+            query.setParameter("cName", name);
+
+            List<Customer> list = query.list();
+            for (Customer customer : list) {
+                System.out.println(customer);
+            }
+        }
+    }
+}
